78a1918f30b503b6b1e2e7f9d0ab454829e2e45f
Sanaa Amer <sa3375@fayoum.edu.eg>
2026-08-05 22:22:53 +0300

    feat: Add redirect from root path to login page

--- small-ecommerce-app/src/routes/AppRoutes.jsx
+++ small-ecommerce-app/src/routes/AppRoutes.jsx
@@ -5,10 +5,12 @@ import ProductsPage from "../features/products/ProductsPage";
 import ProductDetailsPage from "../features/products/ProductDetailsPage";
 import NotFound from "../pages/NotFound";
 import Layout from "../layouts/Layout";
+import { Navigate } from "react-router-dom";
 
 function AppRoutes() {
     return (
         <Routes>
+            <Route path="/" element={<Navigate to="/login" replace />} />
             <Route path="/login" element={<LoginPage />} />
             <Route element={<ProtectedRoute />}>
             <Route element={<Layout />}>
